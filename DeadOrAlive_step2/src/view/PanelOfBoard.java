3a729db51fac54f3edf2d372812edde354be3d8e
rkdntm1 <rkdntm1@gmail.com>
2021-04-15 17:49:33 +0900

    step2

--- DeadOrAlive_step2/src/view/PanelOfBoard.java
+++ DeadOrAlive_step2/src/view/PanelOfBoard.java
@@ -13,15 +13,15 @@ import model.Board;
 public class PanelOfBoard extends JPanel {
 	private Board board;
 	// 캐릭터 아이콘 만들어주기
-	ImageIcon PlayerIcon = new ImageIcon("Character.jpg");
+	ImageIcon playerIcon = new ImageIcon("Character.jpg");
 	JLabel lbl4Player = new JLabel();
 
 	PanelOfBoard(Board board) {
 		this.board = board;
 		this.setLayout(null); // 어디든 만들어지도록 레이아웃 null
 
-		lbl4Player.setIcon(PlayerIcon); // 아이콘 설치
-		lbl4Player.setLocation(Board.WIDTH / 2 - (PlayerIcon.getIconWidth() / 2), Board.HEIGHT - PlayerIcon.getIconHeight()); // 아이콘 초기 위치 설정 (맨아래 중앙)
+		lbl4Player.setIcon(playerIcon); // 아이콘 설치
+		lbl4Player.setLocation(Board.WIDTH / 2 - (playerIcon.getIconWidth() / 2), Board.HEIGHT - playerIcon.getIconHeight()); // 아이콘 초기 위치 설정 (맨아래 중앙)
 		System.out.println("Player = " + lbl4Player.getX());
 		lbl4Player.setSize(200, 150); // 아이콘 size 정의
 
