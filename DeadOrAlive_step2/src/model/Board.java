ba75c929527d4698df6ea364bcbf728ae1b2c14e
rkdntm1 <rkdntm1@gmail.com>
2021-04-18 23:11:21 +0900

    step2

--- DeadOrAlive_step2/src/model/Board.java
+++ DeadOrAlive_step2/src/model/Board.java
@@ -5,19 +5,25 @@ import java.util.List;
 
 public class Board {
 	// 보드 크기
-	public static final int WIDTH = 1000;
-	public static final int HEIGHT = (int) (WIDTH / 1.6);
+	public static final int HEIGHT = 1000;
+	public static final int WIDTH = (int) (HEIGHT / 1.6);
 	// 캐릭터 기본 위치
 	public static final int XPOSOFCHARACTER = WIDTH / 2;
 	// 한 면을 걸을 때 걸리는 총 시간(초)
-	private static final int TIME = 80;
+	private static final int TIME_BOMB_APPLE = 300;
+	private static final int TIME_CHARACTER = 30;
 	// 면의 길이와 시간으로 산출한 속도.
-	public static final int SPEED_BOOM_APPLE = HEIGHT / TIME;
-	public static final int SPEED_CHARACTER = WIDTH / TIME;
+	public static final int SPEED_BOMB_APPLE = HEIGHT / TIME_BOMB_APPLE;
+	public static final int SPEED_PLAYER = WIDTH / TIME_CHARACTER;
 
 	private List<Thing> things = new ArrayList<>();
-
+	private Player player;
+	private Apple apple;
+	private Bomb bomb;
+	
 	public void load(Thing thing) { // player, bomb, apple을 통칭시켜서 기억
 		things.add(thing);
 	}
+	
+	
 }
--- DeadOrAlive_step2/src/view/PanelOfBoard.java
+++ DeadOrAlive_step2/src/view/PanelOfBoard.java
@@ -1,55 +1,68 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
+import controll.DeadOrAliveController;
+import model.Apple;
 import model.Board;
+import model.Bomb;
+import model.Player;
 
 public class PanelOfBoard extends JPanel {
-	private Board board;
-	// 캐릭터 아이콘 만들어주기
-	ImageIcon playerIcon = new ImageIcon("Character.png");
+	private JLabel lbl4Score;
+	private JButton btnStart;
+
+	// 캐릭터 아이콘 가져오기
+	ImageIcon PlayerIcon = new ImageIcon("Character.png");
+
+	// 아이콘을 담을 라벨 만들기
 	JLabel lbl4Player = new JLabel();
 
-	PanelOfBoard(Board board) {
-		this.board = board;
-		this.setLayout(null); // 어디든 만들어지도록 레이아웃 null
+	private int basicScore = DeadOrAliveController.BASIC_SCORE;
+	private Player player;
 
-		lbl4Player.setIcon(playerIcon); // 아이콘 설치
-		lbl4Player.setLocation(Board.WIDTH / 2 - (playerIcon.getIconWidth() / 2)
-				, Board.HEIGHT - playerIcon.getIconHeight() - 50); // 아이콘 초기 위치 설정 (맨아래 중앙)
-		System.out.println("Player = " + lbl4Player.getX());
+	PanelOfBoard(Player player) {
+		this.player = player;
+
+		this.setLayout(null); // 어디든 만들어지도록 레이아웃 null\
+
+		// 플레이어 아이콘설정
+		lbl4Player.setIcon(PlayerIcon); // 아이콘 설치
 		lbl4Player.setSize(200, 150); // 아이콘 size 정의
-
+		lbl4Player.setLocation(player.getxPos(), player.getyPos() - lbl4Player.getHeight() / 2); // 아이콘 초기 위치 설정 (맨아래 중앙)
 		this.add(lbl4Player);
-		
-		// 좌우 움직일 수 있도록 keyListener 장착  
+
 		this.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e){
+			public void keyPressed(KeyEvent e) {
 				switch (e.getKeyCode()) {
 				case KeyEvent.VK_LEFT:
-					if (lbl4Player.getX() < -(lbl4Player.getWidth() / 7)) {
-						lbl4Player.setLocation(lbl4Player.getX() + 20, lbl4Player.getY());
+					if (player.getxPos() < 0) {
+						player.setxPos(player.getxPos() + 20);
 					}
-					lbl4Player.setLocation(lbl4Player.getX() - Board.SPEED_CHARACTER, lbl4Player.getY());
+					player.setxPos(player.getxPos() - Board.SPEED_PLAYER);
 					break;
 				case KeyEvent.VK_RIGHT:
-					if (lbl4Player.getX() > Board.WIDTH - lbl4Player.getWidth()) {
-						lbl4Player.setLocation(lbl4Player.getX() - 20, lbl4Player.getY());
+					if (player.getxPos() > Board.WIDTH - lbl4Player.getWidth() + 20) {
+						player.setxPos(player.getxPos() - 20);
 					}
-					lbl4Player.setLocation(lbl4Player.getX() + Board.SPEED_CHARACTER, lbl4Player.getY());
+					player.setxPos(player.getxPos() + Board.SPEED_PLAYER);
 					break;
 				}
-				
+				lbl4Player.setLocation(player.getxPos(), player.getyPos() - lbl4Player.getHeight() / 2);
 			}
 		});
 		this.requestFocus();
 		this.setFocusable(true);
 	}
-}
 
+}
